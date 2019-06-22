c7072c475a97a244d53f347baddd722b93c4577d
Artem Podustov <artem.podustov@xyrality.com>
2019-06-22 07:31:05 +0200

    Initial Commit

---
+++ LastFM Test App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LastFM Test App
+//
+//  Created by Artem Podustov on 6/22/19.
+//  Copyright © 2019 podustov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
